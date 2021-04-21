184459b4b4c31928f6e2648fa27e76b5c3ed704c
dala <dala_88@hotmail.com>
2021-04-21 14:35:40 +0200

    Can create newsletter

--- mvc2StefanSupperShopp/mvc2StefansSuperShop-master/StefanShopWeb/Controllers/NewsLetterController.cs
+++ mvc2StefanSupperShopp/mvc2StefansSuperShop-master/StefanShopWeb/Controllers/NewsLetterController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StefanShopWeb.Data;
+using StefanShopWeb.Models;
 using StefanShopWeb.ViewModels;
 
 namespace StefanShopWeb.Controllers
@@ -27,6 +29,37 @@ namespace StefanShopWeb.Controllers
                 }).ToList();
 
             return View(model);
+        }
+
+        [Authorize(Roles = "Admin, Product Manager")]
+        public IActionResult New()
+        {
+            var viewModel = new NewsLetterNewViewModel();
+
+
+            return View(viewModel);
+        }
+
+
+        [Authorize(Roles = "Admin, Product Manager")]
+        [HttpPost]
+
+        public IActionResult New(NewsLetterNewViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbNewsLetter = new NewsLetter();
+                dbContext.NewsLetters.Add(dbNewsLetter);
+
+
+                dbNewsLetter.Title = viewModel.Title;
+                dbNewsLetter.Content = viewModel.Content;
+                dbNewsLetter.Id = viewModel.Id;
+
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 
---
+++ mvc2StefanSupperShopp/mvc2StefansSuperShop-master/StefanShopWeb/ViewModels/NewsLetterNewViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StefanShopWeb.ViewModels
+{
+    public class NewsLetterNewViewModel
+    {
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        public Guid Id { get; set; }
+
+    }
+}
