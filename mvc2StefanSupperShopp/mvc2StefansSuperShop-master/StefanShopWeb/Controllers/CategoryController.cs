caabf7c22b39466c47feecfdd5e89c5c38b381c7
dala <dala_88@hotmail.com>
2021-05-13 12:23:41 +0200

    category

---
+++ mvc2StefanSupperShopp/mvc2StefansSuperShop-master/StefanShopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StefanShopWeb.Data;
+using StefanShopWeb.ViewModels;
+
+namespace StefanShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public IActionResult Index(string q)
+        {
+            var viewModel = new CategoryIndexViewModel();
+
+            viewModel.Products = dbContext.Products
+                .Include(r=> r.Category)
+                .Where(r => q == null || r.CategoryId.ToString().Equals(q))
+                .Select(categories => new CategoryIndexViewModel()
+                {
+                    CategoryName = categories.Category.CategoryName,
+                    CategoryId= categories.Category.CategoryId,
+
+                }).ToList();
+            return View(viewModel);
+        }
+    }
+}
---
+++ mvc2StefanSupperShopp/mvc2StefansSuperShop-master/StefanShopWeb/ViewModels/CategoryIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using StefanShopWeb.Models;
+
+namespace StefanShopWeb.ViewModels
+{
+    public class CategoryIndexViewModel
+    {
+        public string q { get; set; }
+
+        [Key]
+        [Column("CategoryID")]
+        public int CategoryId { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        [Column(TypeName = "image")]
+        public byte[] Picture { get; set; }
+
+        [InverseProperty("Category")]
+        public virtual List<CategoryIndexViewModel> Products { get; set; }
+    }
+}
